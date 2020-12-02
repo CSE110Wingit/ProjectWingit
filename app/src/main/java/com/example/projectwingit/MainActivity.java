2de39be01fc2168619fe21b4ee4eee8292191e3a
Wingit <Cse110Wingit@gmail.com>
2020-12-02 14:36:00 -0800

    Update to MainActivity.java
    Fixed bug with url.

--- app/src/main/java/com/example/projectwingit/MainActivity.java
+++ app/src/main/java/com/example/projectwingit/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
