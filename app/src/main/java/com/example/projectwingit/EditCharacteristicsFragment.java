cabbb4002fb7f53e8fb1fa52ba1559b0d21314c9
henry lister <hlister@ucsd.edu>
2020-12-06 14:12:37 -0800

    user characteristics page

---
+++ app/src/main/java/com/example/projectwingit/EditCharacteristicsFragment.java
@@ -0,0 +1,109 @@
+package com.example.projectwingit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+// fragment for displaying the edit characteristics page, but most of the code for changing profile
+// values is located in UserAccount.java, such as the listeners for the ui components
+public class EditCharacteristicsFragment extends Fragment{
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public EditCharacteristicsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EditCharacteristicsFragment
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EditCharacteristicsFragment newInstance(String param1, String param2) {
+        EditCharacteristicsFragment fragment = new EditCharacteristicsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_edit_characteristics, container, false);
+
+        // setting listeners for the edit preference sweet and spicy seekbars
+        SeekBar spiceSB = v.findViewById(R.id.spice_seekbar);
+        spiceSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                TextView spiceTV = v.findViewById(R.id.spice_textview);
+                spiceTV.setText("Spice Level Preference: " + progress);
+                UserAccount.prefSpiciness = progress;
+            }
+        });
+
+        SeekBar sweetSB = v.findViewById(R.id.sweet_seekbar);
+        sweetSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                TextView sweetTV = v.findViewById(R.id.sweetness_textView);
+                sweetTV.setText("Sweetness Level Preference: " + progress);
+                UserAccount.prefSweetness = progress;
+            }
+        });
+
+        return v;
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/projectwingit/UserAccount.java
+++ app/src/main/java/com/example/projectwingit/UserAccount.java
@@ -10,24 +10,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import com.example.projectwingit.io.LambdaRequests;
+import com.example.projectwingit.io.LambdaResponse;
+import com.example.projectwingit.utils.LoginInfo;
 import com.google.android.material.navigation.NavigationView;
+
 
 public class UserAccount extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private NavigationView accountView;
     private DrawerLayout account_Drawer;
 
+    private String loginUsername;
+    private String loginEmail;
+
+    // preference values for the editCharacteristics page (bools default to false, ints default to -1)
+    boolean prefNutAllergy = false;
+    boolean prefGlutenFree = false;
+    boolean prefVegan = false;
+    boolean prefVegetarian = false;
+    static int prefSpiciness = -1;
+    static int prefSweetness = -1;
+
+    boolean isLoggedIn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+
+        // checks if user is logged in
+        if (LoginInfo.CURRENT_LOGIN != null) {
+            loginUsername = LoginInfo.CURRENT_LOGIN.username;
+            loginEmail = LoginInfo.CURRENT_LOGIN.email;
+            isLoggedIn = true;
+        }
+
 
         account_Drawer = findViewById(R.id.drawer_Acc);
         account_Drawer.openDrawer(GravityCompat.START);
         NavigationView accountView = findViewById(R.id.userAccountView);
         accountView.setNavigationItemSelectedListener(this);
 //        getSupportFragmentManager().beginTransaction().replace(R.id.container_user_account, new LoginFragment()).commit();
+
+
+
 
     }
 
@@ -41,9 +74,90 @@ public class UserAccount extends AppCompatActivity implements NavigationView.OnN
             case R.id.user_account_create:
                 getSupportFragmentManager().beginTransaction().replace(R.id.container_user_account, new RegisterFragment()).commit();
                 break;
+            case R.id.user_account_characteristics:
+                // only users with accounts can access TODO set to true to debug characteristics page
+                if(isLoggedIn) getSupportFragmentManager().beginTransaction().replace(R.id.container_user_account, new EditCharacteristicsFragment()).commit();
+                break;
         }
         account_Drawer.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+
+    // checkbox listener for preference checkboxes
+    // needs to be in activity and not fragment code
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox)view).isChecked();
+        switch(view.getId()) {
+            case R.id.checkbox_gluten_free:
+                if (checked) prefGlutenFree = true;
+                else prefGlutenFree = false;
+                break;
+
+            case R.id.checkbox_nut_allergy:
+                if (checked) prefNutAllergy = true;
+                else prefNutAllergy = false;
+                break;
+
+            case R.id.checkbox_vegan:
+                if (checked) prefVegan = true;
+                else prefVegan = false;
+                break;
+
+            case R.id.checkbox_vegetarian:
+                if (checked) prefVegetarian = true;
+                else prefVegetarian = false;
+                break;
+        }
+    }
+
+
+
+    // listener for the confirm button in edit preferences that calls the lambda method to
+    // actually change a user's preferences on the backend
+    public void onConfirmClicked(View view){
+
+        // TODO call lambda method to confirm current changes
+        LambdaResponse recipe = LambdaRequests.editPersonalCharacteristics(loginUsername, loginEmail, prefNutAllergy, prefGlutenFree, prefSpiciness);
+    }
+
+    // resets all of the preference fields to their default values, tells the backend
+    public void onResetClicked(View view){
+        Button resetButton = findViewById(R.id.prefResetButton);
+
+        prefVegetarian = false;
+        prefVegan = false;
+        prefGlutenFree = false;
+        prefNutAllergy = false;
+        prefSpiciness = -1;
+        prefSweetness = -1;
+
+        CheckBox veganCB = findViewById(R.id.checkbox_vegan);
+        veganCB.setChecked(false);
+
+        CheckBox vegetarianCB = findViewById(R.id.checkbox_vegetarian);
+        vegetarianCB.setChecked(false);
+
+        CheckBox glutenCB = findViewById(R.id.checkbox_gluten_free);
+        glutenCB.setChecked(false);
+
+        CheckBox nutCB = findViewById(R.id.checkbox_nut_allergy);
+        nutCB.setChecked(false);
+
+        SeekBar spiceSB = findViewById(R.id.spice_seekbar);
+        spiceSB.setProgress(0);
+
+        SeekBar sweetSB = findViewById(R.id.sweet_seekbar);
+        sweetSB.setProgress(0);
+
+        TextView spiceTB = findViewById(R.id.spice_textview);
+        spiceTB.setText("Spice Level Preference: None");
+
+        TextView sweetTB = findViewById(R.id.sweetness_textView);
+        sweetTB.setText("Sweetness Level Preference: None");
+
+
+        // TODO calls lambda method on reset
+        LambdaResponse recipe = LambdaRequests.editPersonalCharacteristics(loginUsername, loginEmail, false, false, -1);
     }
 }
